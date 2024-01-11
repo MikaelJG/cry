ac0354d74f0c1c938e1297e779eb86ee6f9d7f91
MikaelJG <mjgonsalves@protonmail.com>
2024-01-10 22:03:59 -0500

    making progress

--- path.cr
+++ path.cr
@@ -1,25 +1,45 @@
-path = "example.txt"
+def find_todays_files(note_dir : String, date : String, ext : String) : Array(String)
+  todays_files = [] of String
+  Dir.glob("#{note_directory}/**/#{date}*#{file_extension}").each do |file|
+    todays_file << file
+  end
+  todays_files 
+end
+
+# Path["~/bin"].expand(home: true) # => Path["/home/crystal/bin"]
+
+nb_dir = Path["~/.nb"].expand(home: true)
+
+files = [] of String
+
+Dir.each_child(nb_dir) do |x|
+    files << x
+    puts "Got #{x}" # refuses . and .. as dir
+end
+
+# lines = [] of String
+# idx = 0
+# 
+# File.each_line(path, chomp: true) do |line|
+#     lines << line
+# end
+# 
+# lines.each do | line |
+#     idx += 1
+#     puts "Line #{idx}: #{line} "
+# end
+
+# d = Dir.new("testdir")
+# d.each_child do |x|
+#     files << x
+#     puts "Got #{x}" # refuses . and .. as dir
+# end
 
 # create new file and write to it
 
-file = File.new path, "w"
-file.puts "first line ever"
-file.puts "second line ever"
-file.close
-
-
-lines = [] of String
-idx = 0
-
-# file = File.open path, "w"
-# file.puts "a line"
+# file = File.new path, "w"
+# file.puts "first line ever"
 # file.close
-
-File.each_line(path, chomp: true) do |line|
-    lines << line
-end
-
-lines.each do | line |
-    puts line 
-end
-
+# 
+# Dir.mkdir("testdir")
+# File.write("testdir/a.h", "") # touch file
